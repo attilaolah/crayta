760748fbd7a098a542b5f1f212ef99615eee803a
Attila Oláh <atl@google.com>
2021-03-28 17:59:41 +0200

    Check in terrain_generator.lua.
    An example Lua script that generates a terrain at 5cm precision using interleaved voxel meshes.

---
+++ terrain_generator.lua
@@ -0,0 +1,60 @@
+-- Voxel scale, i.e. width of a voxel in world units (i.e. cm).
+local S = 25
+
+-- Vector representing the vertical resolution.
+-- X and Y axes should be zero, Z axis has to devide S.
+local R = Vector.New(0, 0, 5)
+
+-- Generator class.
+local TerrainGenerator = {}
+
+TerrainGenerator.Properties = {
+	{name = "grass", type = "voxelasset", tooltip = "Grass asset"},
+}
+
+function TerrainGenerator:Init()
+  local vm = self:GetEntity()
+  assert(vm:IsA(VoxelMesh), "TerrainGenerator must be assigned to a VoxelMesh instance!")
+
+  Print("[D] " .. vm:GetName() .. " initialising...")
+  
+  local z0 = vm:GetChildren()[1]
+  assert(z0, "TerrainGenerator child is nil!")
+  assert(z0:IsA(VoxelMesh), "TerrainGenerator child must be a VoxelMesh instance!")
+
+  -- Create a separate voxel mesh for each offset.
+  -- This is used to smooth out the terrain a little.
+  self.zmap = {[0] = z0}
+  for step = R.z, S - R.z, R.z do
+    Print("[D] Cloning child voxel mesh to zmap[" .. step .. "].")
+    self.zmap[step] = z0:Clone()
+  end
+
+  for x = 0, 64 do
+    for y = 0, 64 do
+      -- The desired height at (x, y).
+      -- In this example, we simply draw circular steps.
+      local h = math.floor(math.sqrt((R.z*x)^2 + (R.z*y)^2))
+      h = h - h % R.z  -- rown down to the correct resolution
+
+      --Print("(x, y, z): (" .. x .. "," .. y .. "," .. h .. ")")
+      local offset = h % S
+      -- Select the voxel mesh that will contain this column:
+      local mesh = self.zmap[offset]
+
+      --mesh:SetVoxel(Vector.New(S*x, S*y, h), self.properties.grass)
+      for z = 0, h, S do
+        mesh:SetVoxel(Vector.New(S*x, S*y, z), self.properties.grass)
+      end
+    end
+  end
+  
+  for step, mesh in pairs(self.zmap) do
+    -- Once the pixels are set, change the relative position of each sub-mesh:
+    mesh:SetRelativePosition(R*(step / R.z))
+  end
+
+  Print("[D] " .. vm:GetName() .. " all set!")
+end
+
+return TerrainGenerator
